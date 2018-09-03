bb5c0c608ea7bfdfa0194c7723409e921d5b6cc7
OPzCatchMee <cmnguyen999@gmail.com>
2018-09-02 21:23:33 -0500

    Delete GUI display and players can now choose their own colors

--- Main.java
+++ Main.java
@@ -1,30 +1,78 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
+import java.util.Vector;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    Object[] obNumPlayer = {"2", "3", "4", "5", "6"}; // Option for number of players
-    Players[] players = new Players[6];
+    String sColorOption[] = {"Red", "Green", "Blue", "Purple", "Orange", "Yellow", "Teal"};
+    int nNumPlayers = 0;
+    int nColor = 0;
 
+    Players[] players;
 
-    String sNumPlayers = (String)JOptionPane.showInputDialog(
-                    null,
-                    "Please select how many players are playing...",
-                    "Player(s)",
-                    JOptionPane.PLAIN_MESSAGE,
-                    null,
-                    obNumPlayer,
-                    obNumPlayer[0]);
+    boolean bGameRunning = true;
+    Scanner sc;
 
-    for(int p = 0; p < Integer.parseInt(sNumPlayers); p++) {
-      String sName = "Player " + (p+1);
+    System.out.println("=============================================================");
+    System.out.println("============ Welcome to Risk Board Game !!! =================");
+    System.out.println("=============================================================");
 
-      sName = (String)JOptionPane.showInputDialog("Please enter " + sName + " name: ");
-      players[p] = new Players(sName);
+    // Keep running until user pick a valid number of players
+    while(bGameRunning) {
+      if(nNumPlayers >= 2 && nNumPlayers <= 6) {
+        bGameRunning = false;
+      } else {
+        System.out.println("Please select how many players (2-6) are playing...");
+
+        sc = new Scanner(System.in);
+        nNumPlayers = sc.nextInt();
+      }
     }
 
+    System.out.println("=============================================================");
+    System.out.println("============== You have select " + nNumPlayers  + " Players. ===================");
+    System.out.println("=============================================================");
+
+    players = new Players[nNumPlayers];
+
+
+    for(int p = 0; p < nNumPlayers; p++) {
+      String sName = "Player " + (p+1);
+
+      System.out.println("Please name "+ sName + "...");
+      sc = new Scanner(System.in);
+      sName = sc.nextLine();
+
+      bGameRunning = true;
+      do{
+        if(nColor >= 1 && nColor <= sColorOption.length && sColorOption[nColor-1] != null) {
+          bGameRunning = false;
+        } else {
+          System.out.println("Please select the color for player " + sName + "...");
+          for(int c = 0; c < sColorOption.length; c++) {
+            if(sColorOption[c] != null)
+              System.out.println((c+1) + ". " + sColorOption[c]);
+          }
+          sc = new Scanner(System.in);
+          nColor = sc.nextInt();
+        }
+      }while(bGameRunning);
+
+      players[p] = new Players(sName, sColorOption[nColor-1]);
+      sColorOption[nColor-1] = null;
+
+      System.out.println();
+      System.out.println("=============================================================");
+    }
+
+    for(int p = 0; p < nNumPlayers; p++) {
+      System.out.println("=============================================================");
+      System.out.println("Player " + (p+1) + " is name: " + players[p].getName());
+      System.out.println("Player " + (p+1) + " is color: " + players[p].getColor());
+      System.out.println("=============================================================");
+    }
 
   }
-
 }
--- Players.java
+++ Players.java
@@ -1,9 +1,26 @@
 public class Players {
 
   String sName;
+  String sColor;
 
-  Players(String name) {
-    sName = name;
+  Players(String name, String color) {
+    this.sName = name;
+    this.sColor = color;
+  }
+
+  public String getName() {
+    return this.sName;
+  }
+
+  public String getColor() {
+    return this.sColor;
+  }
+
+  public void displayInfo() {
+    System.out.println("=============================================================");
+    System.out.println("Name: " + this.sName);
+    System.out.println("Color: " + this.sColor);
+    System.out.println("=============================================================");
   }
 
 }
