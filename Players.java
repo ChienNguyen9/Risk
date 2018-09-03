692c0099c63c7621604ca175eecb9102638ef5b6
OPzCatchMee <cmnguyen999@gmail.com>
2018-09-03 17:24:49 -0500

    Added getter and setter for dice

--- Players.java
+++ Players.java
@@ -2,10 +2,12 @@ public class Players {
 
   String sName;
   String sColor;
+  int nDice;
 
   Players(String name, String color) {
     this.sName = name;
     this.sColor = color;
+    this.nDice = 0;
   }
 
   public String getName() {
@@ -14,6 +16,14 @@ public class Players {
 
   public String getColor() {
     return this.sColor;
+  }
+
+  public void setDice(int dice) {
+    this.nDice = dice;
+  }
+
+  public int getDice() {
+    return this.nDice;
   }
 
   public void displayInfo() {
