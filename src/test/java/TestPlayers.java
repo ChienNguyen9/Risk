feaffd62fa2f9077c57e5e63ef47cf8a51baaed4
OPzCatchMee <cmnguyen999@gmail.com>
2018-10-05 19:09:50 -0500

    Adding Dice and player test case

--- src/test/java/TestPlayers.java
+++ src/test/java/TestPlayers.java
@@ -7,9 +7,21 @@ import java.util.HashMap;
 public class TestPlayers {
 
   @Test
-  public void setnumofterritoriesFortyThreeTest(){
+  public void initPlayerGainAmries(){
 		Players player = new Players("Georgy", 1000);
 		player.gainArmies(10);
+    player.displayInfo();
+    player.getNumOfArmies();
+    player.getName();
+	}
+
+  @Test
+  public void initPlayerLoseAmries(){
+		Players player = new Players("Georgy", 200);
+		player.loseArmies(10);
+    player.displayInfo();
+    player.getNumOfArmies();
+    player.getName();
 	}
 
 }
