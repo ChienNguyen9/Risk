090ab7abb48e23b7792d2dde08799082710939e9
chance <chancemorris77295@gmail.com>
2018-10-06 11:33:43 -0500

    Deck test cases

--- src/test/java/TestDeck.java
+++ src/test/java/TestDeck.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
+import java.beans.Transient;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -16,6 +17,42 @@ import java.io.File;
 //Ignores wild cards and secret mission cards
 public class TestDeck {
 
+    @Test
+    public void addInvalidArmyType() {
+    Deck deck = new Deck();
+     Card invalidCard = new Card("InvalidInfantry", "Alberta");
+     deck.add(invalidCard);
+    }
+
+    @Test
+    public void addInvalidCountry() {
+        Deck deck = new Deck();
+        Card invalidCard = new Card("Infantry", "InvalidAlberta");
+        deck.add(invalidCard);
+    }
+
+    @Test
+    public void addEmptyCard() {
+        Deck deck = new Deck();
+        Card invalidCard = new Card();
+        deck.add(invalidCard);
+     
+    }
+
+    @Test
+    public void addNullCard() {
+        Deck deck = new Deck();
+        Card nullCard = new Card(null, null);
+        deck.add(nullCard);
+     
+    }
+
+    @Test
+    public void testShuffle() {
+        Deck deck = new Deck();
+        deck.shuffle();
+     
+    }
 
 
 }
